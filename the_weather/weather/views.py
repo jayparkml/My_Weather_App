a4e1ba9463276065544ab624f9154dc8f8b9b98a
Sungju Park <parksungju90@gmail.com>
2020-12-19 18:47:46 -0600

    Add files via upload

---
+++ the_weather/weather/views.py
@@ -0,0 +1,71 @@
+import requests
+from django.shortcuts import redirect, render
+from .models import City
+from .forms import CityForm
+
+# Create your views here.
+def index(request):
+    url='http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=e3a23f9afa41b89d0da301c831205d16'
+
+    err_msg = ""
+    message = ''
+    message_class = ''
+
+    if request.method == "POST":
+        form = CityForm(request.POST)
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist in the world.'
+            else:
+                err_msg = "City already exists in the database."
+    
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added successfully.'
+            message_class = 'is-success'
+
+
+    
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'] ,
+        }
+
+        weather_data.append(city_weather)
+
+    print(weather_data)
+
+    context = {
+        'weather_data': weather_data, 
+        'form': form,
+        'message': message,
+        'message_class': message_class,
+        }
+
+    return render(request, 'weather/weather.html', context)
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
